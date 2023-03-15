4751c340ec48e9ad42d0d6dceddcf175d3cb9781
Åmund Henriksen <aamundhe@getacademy.no>
2023-03-15 20:36:10 +0100

    Add project files.

---
+++ FriendFace/Profile.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendFace
+{
+    public class Profile
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Age { get; private set; }
+        public string From { get; private set; }
+
+        public Profile(string firstName, string lastName, int age, string from)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            From = from;
+        }
+
+        public void AddFriend()
+        {
+
+        }
+
+        public void RemoveFriend()
+        {
+
+        }
+
+        public void ShowFriends()
+        {
+
+        }
+    }
+}
---
+++ FriendFace/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FriendFace
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
